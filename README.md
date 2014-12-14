447c743fd1428a23a3c6a4ebfed3d86f1d902dbb
Sandy Walsh <sandy.walsh@rackspace.com>
2014-12-14 10:14:03 -0400

    Still working on the turn resolution stuff

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
 rootkit
 =======
 
-Messing around
+Note: Just messing around with game mechanics for now.
+
+Rootkit is intended to be a Play-By-Email (PBeM) game. 
+
+* Games have multiple players.
+* Players have multiple Servers.
+* Servers are identified by unique IP address (###.###.###.###)
+* Servers have weaknesses. 
+* Players don't know which weaknesses their servers have.
+* Opponents don't know which weaknesses the other player's servers have. 
+* Players don't know the IP address of their opponent's servers.
+* Each turn is called a "cycle".
+* Players can instruct their servers to run programs. 
+* Players can execute one program per server per cycle. 
+* Some program take many cycles to complete.
+* Some programs take effect immediately when executed (eg: Power Off) 
+* Some programs take effect at the end of the cyle. (eg: Power On)
+* Credits can be used to buy new programs, hardware and knowledge. 
+* Everyone starts with three servers. 
+
+Players are hackers and trying to take down or take over the other servers.
+
+Players have tools to help with their hacking.
+
+Right now they have:
+* `nmap` to scan for other servers. The broader your search, the longer it takes. 
+* `probe` to scan a server for weaknesses (including your own)
+* `mine` to mine for credits
+
+Eventually, I'll add things you can buy with your credits.
+
+* Firewalls
+* CPU/Memory/Network upgrades (faster scans/mining/etc)
+* Better hacking software
+* Patches to repair weaknesses
+* Intrusion detection software
+* Viruses you can install to hijack other servers. 
+
+Other commands I'm thinking about are:
+* `udp` - blasts out a UDP multicast message to all servers. These can be used to talk to other players, but also to instruct your viruses to perform special operations. 
+
+Currently, I'm trying to finalize the turn resolution steps. Things are a little messy in there right now. 
+
+The current approach is:
+* `pre-cycle` - things that are done when the cycle starts. All servers run their pre-cycle step first. 
+* `post-cycle` - things that are done at the end of the cycle. Usually this is a noop. 
+* `completed` - things that are done when the program finishes up. This is where the real work is done. Remember that programs could take several cycles to complete. 
+
+More to come ... patches welcome.
+
+
+
--- server/gamemaster.py
+++ server/gamemaster.py
@@ -15,8 +15,8 @@ class Game(object):
         self.players.append(player)
         player.index = len(self.players) - 1
 
-    def set_command(self, player, turn):
-        player.set_command(turn)
+    def set_command(self, server, turn):
+        server.set_command(turn)
 
     def dump(self):
         print "-----------------------------------------------"
@@ -24,16 +24,35 @@ class Game(object):
             player.dump()
 
     def resolve(self):
-        for player in self.players:
-            cmd = player.current_cmd
-            result = "No commands received from %s" % player.handle
-            if cmd:
-                other_players = self._get_other_players(player)
-                result = cmd.execute(player, other_players)
-                cmd.duration -= 1
-                if cmd.duration < 0:
-                    player.current_cmd = None
-            player.last_result = result
+        # Resolution is done in two phases:
+        # Pre-cycle - fast operations, take effect immediately
+        # Post-cycle - slow operations, takes a full cycle
+        try:
+            for player in self.players:
+                for s in player.servers:
+                    cmd = s.current_cmd
+                    pre_result = "noop"
+                    if cmd:
+                        other_players = self._get_other_players(player)
+                        pre_result = cmd.pre_cycle(s, player, other_players)
+                    s.pre_result = pre_result
+
+            for player in self.players:
+                for s in player.servers:
+                    cmd = s.current_cmd
+                    post_result = "noop"
+                    if cmd:
+                        other_players = self._get_other_players(player)
+                        result = cmd.post_cycle(s, player, other_players)
+                        cmd.duration -= 1
+                        if cmd.duration < 0:
+                            result = s.current_cmd.completed(
+                                                s, player, other_players)
+                            s.current_cmd = None
+                    s.post_result = result
+        except BadCommand as e:
+            s.post_result = "Error: %s" % str(e)
+            s.current_cmd = None
 
 
 class Weakness(object):
@@ -55,6 +74,12 @@ class Server(object):
                            Weakness('anonymous telnet'),
                            Weakness('missing patches'),
                            Weakness('SSL.v3')]
+        self.current_cmd = None
+        self.pre_result = None
+        self.post_result = None
+
+    def set_command(self, command):
+        self.current_cmd = command
 
     def __str__(self):
         return str(self.ip)
@@ -64,28 +89,24 @@ class Server(object):
         for w in self.weaknesses:
             if player.index in w.found_by:
                 print "    '%s' weakness" % w
+        if self.pre_result:
+            print "   > %s" % self.pre_result
+        if self.post_result:
+            print "   < %s" % self.post_result
+        if self.current_cmd:
+            print "    %d cycle(s) left" % self.current_cmd.duration
 
 
 class Player(object):
     def __init__(self, handle):
         self.handle = handle
         self.index = -1  # Not added
-        self.current_cmd = None
-        self.servers = [Server()]
+        self.servers = [Server() for x in range(3)]
         self.known_servers = []
-        self.is_free = True
-        self.last_result = None
         self.credits = 1
-
-    def set_command(self, command):
-        self.current_cmd = command
 
     def dump(self):
         print "%s (%d): %d credits" % (self.handle, self.index, self.credits)
-        if self.last_result:
-            print "    %s" % self.last_result
-        if self.current_cmd:
-            print "    %d cycle(s) left" % self.current_cmd.duration
         for server in self.servers:
             print "   ",
             server.dump(self)
@@ -95,42 +116,62 @@ class Player(object):
                 print "   ",
                 k.dump(self)
 
+    def get_free_servers(self):
+        return [s for s in self.servers if not s.current_cmd]
+
+    def get_busy_servers(self):
+        return [s for s in self.servers if s.current_cmd]
+
+
 class Command(object):
     def __init__(self):
         self.when = datetime.datetime.utcnow()
+        # Duration > 1 : still busy
+        # Duration == 1 : time to do work
+        # Duration == 0 : done
         self.duration = 0
+
+    def pre_cycle(self, server, player, other_players):
+        return "noop"
+
+    def post_cycle(self, server, player, other_players):
+        return "noop"
+
+    def completed(self, server, player, other_players):
+        return "noop"
+
+
+class BadCommand(Exception):
+    pass
 
 
 class NMap(Command):
     def __init__(self, mask):
         super(NMap, self).__init__()
         self.mask = mask
-        self.path = None
+        parts = self.mask.split(".")
+        duration = 0
+        path = []
+        for p in parts:
+            p = p.strip()
+            if p == '*':
+                duration += 1
+                path.append(p)
+            else:
+                address = int(p)
+                path.append(address)
+        self.path = path
+        self.duration = duration
 
-    def execute(self, player, other_players):
-        if not self.path:
-            parts = self.mask.split(".")
-            duration = 0
-            path = []
-            for p in parts:
-                p = p.strip()
-                if p == '*':
-                    duration += 1
-                    path.append(p)
-                else:
-                    address = int(p)
-                    path.append(address)
-            self.path = path
-            self.duration = duration
-
+    def pre_cycle(self, server, player, other_players):
         if len(self.path) != 4:
             self.duration = 0
-            return "Invalid nmap mask given %s" % self.path
+            raise BadCommand("Invalid nmap mask given %s" % self.path)
 
-        if self.duration > 1:
-            return "%s running 'nmap' against %s" % (
-                            player.handle, self.path)
+        return "%s running 'nmap' against %s" % (
+                        player.handle, self.path)
 
+    def completed(self, server, player, other_players):
         choices = []
         for p in other_players:
             for s in p.servers:
@@ -150,32 +191,32 @@ class NMap(Command):
 
 
 class Mine(Command):
-    def execute(self, player, other_players):
+    def execute(self, server, player, other_players):
         player.credits += 1
         self.duration = 1
-        return "%s mined for 1 credit" % (player.handle)
+        return "%s mined for 1 credit on %s" % (player.handle, server)
 
 
 class Probe(Command):
-    def __init__(self, server):
-        self.server = server
+    def __init__(self, target):
+        self.target = target
         self.duration = 2
 
-    def execute(self, player, other_players):
+    def execute(self, server, player, other_players):
         # TODO(sandy): Duration should be based on the quality
         # of the tools you have.
         if self.duration > 1:
-            return "Probing %s" % self.server
-        if self.server.weaknesses:
-            weakness = random.choice(self.server.weaknesses)
+            return "Probing %s" % self.target
+        if self.target.weaknesses:
+            weakness = random.choice(self.target.weaknesses)
             if player.index in weakness.found_by:
                 weakness = None
             else:
                 weakness.found_by.append(player.index)
             if weakness:
-                return "Found '%s' weakness on %s" % (weakness, self.server)
+                return "Found '%s' weakness on %s" % (weakness, self.target)
 
-        return "No new weakness found on %s" % self.server
+        return "No new weakness found on %s" % self.target
 
 
 if __name__ == '__main__':
@@ -191,7 +232,8 @@ if __name__ == '__main__':
 
     all_players = [bob, mary, fred, sue]
     for p in all_players:
-        game.set_command(p, NMap("*.*.*.*"))
+        for s in p.get_free_servers():
+            game.set_command(s, NMap("*.*.*.*"))
 
     game.dump()
 
@@ -200,12 +242,14 @@ if __name__ == '__main__':
         game.dump()
 
     for p in all_players:
-        game.set_command(p, Mine())
+        for s in p.get_free_servers():
+            game.set_command(s, Mine())
     game.resolve()
     game.dump()
 
     for p in all_players:
-        game.set_command(p, Probe(p.known_servers[0]))
+        for s in p.get_free_servers():
+            game.set_command(s, Probe(p.known_servers[0]))
     for x in range(2):
         game.resolve()
         game.dump()
